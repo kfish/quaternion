1e0b1bffd79db49336caef03f9b8e7f8e13dea00
Conrad Parker <conrad@metadecks.org>
2017-05-19 13:34:04 +0800

    tests: add getter/setter tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -16,15 +16,22 @@ all =
             [ test "Unit" <|
                 \() ->
                     qnEqual unit (fromScalar 1)
+            , test "i=7" <|
+                \() ->
+                    floatEqual 7.0 (getI (setI 7 unit))
             ]
         , describe "Getter/setter tests"
-            [ fuzz Fuzz.float "(fromscalar >> getScalar) == identity" <|
+            [ fuzz Fuzz.float "(fromScalar >> getScalar) q == q" <|
                 \f -> (fromScalar >> getScalar) f |> floatEqual f
+            , fuzz Fuzz.float "(fromscalar >> getScalar) q == q" <|
+                \f -> (fromScalar >> getScalar) f |> floatEqual f
+            , fuzz Fuzz.float "setI f q |> getI == f"  <|
+                \f -> (getI (setI 7 unit)) |> floatEqual 7
             ]
         , describe "Identity tests"
-            [ fuzz Fuzz.quaternion "(negate >> negate) == identity" <|
+            [ fuzz Fuzz.quaternion "(negate >> negate) q == q" <|
                 \q -> (Qn.negate >> Qn.negate) q |> qnEqual q
-            , fuzz Fuzz.vec3 "(fromVec3 >> toVec3) == identity" <|
+            , fuzz Fuzz.vec3 "(fromVec3 >> toVec3) v == v" <|
                 \v -> (fromVec3 >> toVec3) v |> vec3Equal v
             ]
         ]
